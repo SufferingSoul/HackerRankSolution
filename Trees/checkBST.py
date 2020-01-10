75d7064980d9c2a0c06b9b8f4e4e0c3cef05c185
leon <873057431@qq.com>
2020-01-10 10:24:11 +0800

    solved checkBST

---
+++ Trees/checkBST.py
@@ -0,0 +1,11 @@
+def checkBST(root):
+    return check(root, -1, 10001)
+
+
+def check(node, minum, maxum):
+    if node is None:
+        return True
+    if node.data >= maxum or node.data <= minum:
+        return False
+    return check(node.left, minum, node.data) and check(node.right, node.data, maxum)
+
